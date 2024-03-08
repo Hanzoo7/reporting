44e0bac1a512cfd65ed2564c84d38c974fb8a373
sebastienf <sebastien.fauquembergue@outlook.com>
2024-03-08 18:16:12 +0100

    v1.0 reportingYaml

--- plugins/modules/writeYaml.py
+++ plugins/modules/writeYaml.py
@@ -1,43 +1,73 @@
 #s.fauquembergue(sii)
 #v1.0
-#initial release 04/03/2024
+#initial release 08/03/2024
+
+#known issue
+# unexpected yaml format provided behavior when datas exceed 1000 lines
 
 from ansible.module_utils.basic import AnsibleModule
 from deepdiff import DeepDiff, Delta
 import os, sys, yaml
 
-def get_resources(dest, nodeName):
-    nodecontent = None
-    destcontent = None
+def get_resources(node, dest):
+    if not os.path.exists(dest):
+        nodeDatas = {}
+    else:
+        nodeDatas=yaml.safe_load((open(dest, 'r')).read())
     
-    if os.path.exists(dest):
-        destcontent=yaml.safe_load((open(dest, 'r')).read())
+    for n in node.split('/'):
+        if not nodeDatas:
+            nodeDatas = {}
         
-        if nodeName in destcontent.keys():
-            nodecontent=destcontent[nodeName]
+        if n not in nodeDatas.keys():
+            nodeDatas[n] = {}
+        
+        nodeDatas=nodeDatas[n]
+    
+    return nodeDatas
 
-    return nodecontent
-
-def test_resources(get,ref):
-    diff=DeepDiff(get, ref ,ignore_order=True, report_repetition=True)
+def test_resources(destNodeDatas, insert) :
+    diff = DeepDiff(destNodeDatas, insert, ignore_order=True, report_repetition=True)
     return diff
 
-def set_resources(ref, dest, diff, nodeName):
-    destcontent=yaml.safe_load((open(dest, 'r')).read())
-    destcontent.update({nodeName:ref})
+def set_resources(insert, node, dest, diff):
+    if not os.path.exists(dest):
+        open(dest, 'w').close()
+
+    nodeDatas_r = yaml.safe_load((open(dest, 'r')).read())
+    position = "nodeDatas_r"
+
+    if not nodeDatas_r:
+        nodeDatas_r = {}
+        
+
+    for n in node.split('/'):
+        exec("if not "+position+":"+position+"={}") 
+        exec("if '"+n+"' not in "+position+".keys():"+position+"['"+n+"']={}")    
+        position = position+"['"+n+"']"
+        
+    exec(position+"=insert")
+
+    
+    nodeDatas_w = open(dest, 'w')
+
+        
+    yaml.dump(nodeDatas_r, nodeDatas_w, default_flow_style=False, encoding = "utf-8", allow_unicode=True)
+    #nodeDatas_w.write(str(objet_python))
 
 
-    #Write to file
 
 
 
-    return destcontent
+    nodeDatas_w.close()
+    return dict(diff)
 
 def main():
     #region params
     module_args = dict(
-        nodeName = dict(required=True, type='str'),
-        content = dict(required=True, type='dict'),
+        node = dict(required=True, type='str'),
+        insertlist = dict(required=False, type='list'),
+        insertdict = dict(required=False, type='dict'),
         dest = dict(required=True, type='str'),
         )
     #endregion
@@ -48,9 +78,15 @@ def main():
         supports_check_mode=True
     )
 
-    nodeName = module.params.get('nodeName')
-    content = module.params.get('content')
+    node = module.params.get('node')
+    insertlist = module.params.get('insertlist')
+    insertdict = module.params.get('insertdict')
     dest = module.params.get('dest')
+
+    if insertlist:
+        insert = insertlist
+    elif insertdict:
+        insert = insertdict
 
     result = dict(
         changed=False,
@@ -60,16 +96,16 @@ def main():
     #endregion
 
     try:       
-        get = get_resources(dest, nodeName)
-        result['message']=get
-        test = test_resources(get=get,ref=content)
+        destNodeDatas = get_resources(node, dest)
+        result['message'] = 'Compliant'
+        diff = test_resources(destNodeDatas, insert)
         
         if module.check_mode:
             result['skipped'] = True
             module.exit_json(**result)        
 
-        if test != {}:
-            set = set_resources(content, dest, test, nodeName)
+        if diff != {}:
+            set = set_resources(insert, node, dest, diff)
             result['message'] = set
             result['changed'] = True
 
